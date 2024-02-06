f916f6945f66bf45e1293877e73be7f49c398626
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2024-02-06 11:08:05 +0100

    Aggiunta gestione db

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 
+const db = new sqlite3.Database('data/memory.db');
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+
