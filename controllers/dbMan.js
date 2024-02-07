e52e7b9bf1a79b2c9d468e7c5a72f5e94316570e
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2024-02-08 00:24:45 +0100

    Implementate funzioni di inserimento dati nel db

--- controllers/dbMan.js
+++ controllers/dbMan.js
@@ -1,51 +1,11 @@
-const match = require('./dbMan/match.js');
-const squad = require('./dbMan/squad.js');
-const sqlite3 = require('sqlite3').verbose();
+const sql = require('sqlite3').verbose();
 const db_pos = 'data/memory.db';
 
 module.exports = {
-
-    example: function () {
-        const db = new sqlite3.Database(":memory:");
-
-        db.serialize(() => {
-            db.run("CREATE TABLE lorem (info TEXT)");
-
-            const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-            for (let i = 0; i < 10; i++) {
-                stmt.run("Ipsum " + i);
-            }
-            stmt.finalize();
-
-            db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-                console.log(row.id + ": " + row.info);
-            });
-        });
-
-        db.close();
-    },
-
-    check: function () {
-        let isthere = false;
-        const db = new sqlite3.Database(db_pos);
-        db.each(`
-            SELECT name  
-            FROM sqlite_master  
-            WHERE  
-                type='table' AND name='match';
-        `,
-        (err, row) => {
-            console.log(row.name);
-            if (row.name === "match") {
-                isthere = true;
-            }
-        });
-        db.close;
-        return(isthere);
-    },
+    position: db_pos,
 
     generate: function () {
-        const db = new sqlite3.Database(db_pos);
+        const db = new sql.Database(db_pos);
         try {
             db.run(`
             CREATE TABLE squad 
@@ -57,6 +17,7 @@ module.exports = {
             db.run(`
             CREATE TABLE match 
             (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 id_squad_a INT,
                 id_squad_b INT,
                 points_squad_a TINYINT,
@@ -72,7 +33,7 @@ module.exports = {
     },
 
     insertExample: function () {
-        const db = new sqlite3.Database(db_pos);
+        const db = new sql.Database(db_pos);
         try {
             db.run(`
             INSERT INTO squad ('name') 
@@ -93,6 +54,63 @@ module.exports = {
             console.log(error);
         }
         db.close;
+    },
+
+    insertNewMatch: function (id_squad_a, id_squad_b, points_squad_a, points_squad_b) {
+        const db = new sql.Database(db_pos);
+        try {
+            const stmt = db.prepare(`
+                INSERT INTO match ('id_squad_a', 'id_squad_b', 'points_squad_a', 'points_squad_b')
+                VALUES
+                    (?, ?, ?, ?)
+                ;
+            `);
+            stmt.run(id_squad_a, id_squad_b, points_squad_a, points_squad_b);
+            stmt.finalize();
+        } catch (error) {
+            console.log(error);
+        }
+        db.close();
+    },
+
+    insertNewSquad: function (sq_name) {
+        const db = new sql.Database(db_pos);
+        try {
+            const stmt = db.prepare(`
+                INSERT INTO squad ('name')
+                VALUES
+                    (?)
+                ;
+            `);
+            stmt.run(sq_name);
+            stmt.finalize();
+        } catch (error) {
+            console.log(error);
+        }
+        db.close();
+    },
+
+    readAllMatches: function () {
+        const db = new sql.Database(db_pos);
+        try {
+            db.each(`
+                SELECT squad_a.name AS nameA, squad_b.name AS nameB, points_squad_a, points_squad_b
+                FROM match
+                INNER JOIN squad AS squad_a ON match.id_squad_a = squad_a.id
+                INNER JOIN squad AS squad_b ON match.id_squad_b = squad_b.id            
+            `, (err, row) => {
+                data = {
+                    'name_squad_a': row.nameA,
+                    'name_squad_b': row.nameB,
+                    'points_squad_a': row.points_squad_a,
+                    'points_squad_b': row.points_squad_b
+                }
+                return data;
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        db.close();
     }
 
 }
--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./controllers/dbMan.js');
+const fs = require("fs");
 
 const app = express();
+
+if (!fs.existsSync(db.position)) {
+    console.log("WARN: File DB è assente, creazione in corso");
+    db.generate();
+    db.insertExample();
+    console.log("  ... Completato");
+}
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-console.log(db.check());
-if(!db.check()){
-    db.generate();
-}
+//db.insertNewMatch(3, 2, 5, 0);
+//db.insertNewSquad("Roma");
+
+//var data = db.readAllMatches();
+//console.log(data.name_squad_a);
